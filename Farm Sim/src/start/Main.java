611cdbb170a5564b540ba1e3d039986f956a8fc6
Collin A Mabus <camabus42@students.tntech.edu>
2015-05-07 18:45:07 -0500

    structure stuff

--- Farm Sim/src/start/Main.java
+++ Farm Sim/src/start/Main.java
@@ -1,29 +1,38 @@
 package start;
 
+import java.io.IOException;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.newdawn.slick.util.ResourceLoader;
 
 public class Main {
 
-	public void start() {
-		try {
-			Display.setDisplayMode(new DisplayMode(800, 600));
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-
-		while (!Display.isCloseRequested()) {
-			Display.update();
-		}
-		
-		Display.destroy();
+	private final String resources = "/res";
+	
+	ResourceLoader res_loader;
+	
+	State gameState = State.INTRO;
+	
+	public void init() throws IOException {
+		res_loader = new ResourceLoader();
 	}
 
+	public void run() {
+		while (!Display.isCloseRequested()) {
+			switch(gameState) {
+			case INTRO:
+				break;
+			case MAIN_MENU:
+				break;
+			case GAME:
+				break;
+			}
+		}
+	}
+	
 	public static void main(String[] argv) {
 		Main main = new Main();
-		main.start();
 	}
 }
---
+++ Farm Sim/src/start/State.java
@@ -0,0 +1,5 @@
+package start;
+
+public enum State {
+INTRO, MAIN_MENU, GAME;
+}
