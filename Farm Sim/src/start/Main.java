bbe9243ff0091aa7ab4a8b2c6e3226159a4696c4
Collin A Mabus <camabus42@students.tntech.edu>
2015-05-07 17:48:47 -0500

    first push

---
+++ Farm Sim/src/start/Main.java
@@ -0,0 +1,29 @@
+package start;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Main {
+
+	public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		while (!Display.isCloseRequested()) {
+			Display.update();
+		}
+		
+		Display.destroy();
+	}
+
+	public static void main(String[] argv) {
+		Main main = new Main();
+		main.start();
+	}
+}
