6e3827ea2176637ad81041b096eaf0259cde9ccc
Cocoa Muffs <linkmabus@hotmail.com>
2015-05-27 13:40:56 -0500

    Map bulder staff

--- Farm Sim/src/game/Mapbuilder.java
+++ Farm Sim/src/game/Mapbuilder.java
@@ -3,14 +3,17 @@ package game;
 import interfaces.Game;
 import interfaces.Variables;
 import interfaces.file.FileManager;
+import interfaces.file.Logging;
+import interfaces.file.Logging.Type;
 
+import java.util.EnumSet;
 import java.util.Random;
 
+import object.Entity.Flag;
 import specifier.Vector2D;
 
 /**
- * Sea - 0 Grass - 1 Lake - 2 River - 3 Sand - 4 Mountain - 5
- * 
+ * Map Buider An exercise in generating random maps
  * 
  * @author Cocoa
  *
@@ -18,6 +21,9 @@ import specifier.Vector2D;
 
 public class Mapbuilder
 {
+	/**
+	 * Enum for Size of Map NOTE: May be deprecated as our size is a game variable although can still be used as random, unsure
+	 */
 	public enum Size
 	{
 		SMALL(512), MEDIUM(1024), LARGE(2048);
@@ -29,30 +35,60 @@ public class Mapbuilder
 		}
 	}
 
+	/**
+	 * MASS An abstract class used to define and detail terrain
+	 */
 	private class Mass
 	{
-		protected int Orientation;
+		protected MassType MT;
+
+		/* North, South, East, West */
+		protected Orientation Orientation;
+		/* Array of tile indices that share borders with a different tile type */
 		protected int Border[];
 
+		/* Ending points of each box created by the mass */
+		/* Size of each array is number of boxes + 1 */
 		protected int EndX[];
 		protected int EndY[];
+		
+		protected Tile.Type TType;
 
-		protected void Init(int Orient)
+		/**
+		 * Initializes and builds the structure of the Terrain Mass
+		 * 
+		 * @param Orient
+		 *            - Map orientation (i.e. North South East West)
+		 */
+		protected void Init(Orientation Orient, Tile.Type T)
 		{
-			Orientation = Orient;
-			EndX = new int[(int) (Math.round(Math.log10((double) Dimension) / Math.log10(2)) / 2) + 1];
-			EndY = new int[(int) (Math.round(Math.log10((double) Dimension) / Math.log10(2)) / 2) + 1];
+			this.Orientation = Orient;
+			this.TType = T;
+			EndX = new int[(int) ((Math.round(Math.log10((double) Dimension) / Math.log10(2))) * 2) + 1];
+			EndY = new int[(int) ((Math.round(Math.log10((double) Dimension) / Math.log10(2)) * 4) / 2) + 1];
 
 			Border = new int[Dimension];
 
-			if (Orientation < 2)
+			// If North or South
+			if (Orientation == Orientation.NORTH || Orientation == Orientation.SOUTH)
 			{
+				
+				System.out.println("EndX length - " + EndX.length + ", EndY length - " + EndY.length);
+				
 				for (int i = 1; i < EndX.length; i++)
 				{
-					EndX[i] = ((Dimension / (EndX.length)) * i);
-					EndY[i] = Rand.nextInt((Max_Mass / 2)) + Max_Mass / 2;
+					// Even sized blocks on X-axis for now
+					EndX[i] = ((Dimension / (EndX.length - 1)) * i);
+					// We generate a random height from half of the maximum mass to the max mass
+					EndY[i] = Rand.nextInt(Max_Mass) + Max_Mass / 2;
+					
+					while (Math.abs(EndY[i - 1] - EndY[i]) < 2)
+						EndY[i] = Rand.nextInt(Max_Mass) + Max_Mass / 2;
+					
+					System.out.println("EndX & EndY @ " + i + " is " + EndX[i] + " " + EndY[i]);
 				}
 			}
+			// If East or West
 			else
 			{
 				for (int i = 1; i < EndX.length; i++)
@@ -62,21 +98,22 @@ public class Mapbuilder
 				}
 			}
 
+			// Initialize the first X & Y index at their starting point
 			switch (Orientation)
 			{
-				case 0: // NORTH
+				case NORTH:
 					EndX[0] = 0;
 					EndY[0] = 0;
 					break;
-				case 1: // SOUTH
+				case SOUTH:
 					EndX[0] = 0;
 					EndY[0] = Dimension - EndY[1];
 					break;
-				case 2: // EAST
+				case EAST:
 					EndX[0] = Dimension - EndX[1];
 					EndY[0] = 0;
 					break;
-				case 3: // WEST
+				case WEST:
 					EndX[0] = 0;
 					EndY[0] = 0;
 					break;
@@ -84,9 +121,14 @@ public class Mapbuilder
 		}
 	}
 
+	private enum Orientation
+	{
+		NORTH, SOUTH, EAST, WEST, CENTER, DEFAULT;
+	}
+
 	public enum MassType
 	{
-		SEA, MOUNTAIN, LAKE
+		SEA, MOUNTAIN, LAKE;
 	}
 
 	private Size Size;
@@ -94,15 +136,15 @@ public class Mapbuilder
 	private Map Map;
 	private int Max_Mass;
 	private Random Rand;
+	private EnumSet<Orientation> Orientations;
 
-	private int Shore[];
-
-	private int sea;
+	private Mass Ocean;
+	private Mass Mountain;
 
 	public Mapbuilder()
 	{
 		Rand = new Random(System.currentTimeMillis());
-		int Random = Rand.nextInt(3);
+		// int Random = Rand.nextInt(3);
 		// change depending on random
 
 		Dimension = Size.value;
@@ -110,31 +152,40 @@ public class Mapbuilder
 
 	public Mapbuilder(Size S)
 	{
-		Rand = new Random(System.currentTimeMillis());
-		Dimension = (int) Variables.GetInstance().Get("m_width").Current();
-		Max_Mass = Dimension / 16;
-
-		Shore = new int[Dimension];
+		this.Rand = new Random(System.currentTimeMillis());
+		this.Dimension = (int) Variables.GetInstance().Get("m_width").Current();
+		this.Max_Mass = Dimension / 16;
+		this.Orientations = EnumSet.noneOf(Orientation.class);
+		this.Ocean = new Mass();
 
 		Map = game.Map.GetInstance();
 	}
 
 	public void init()
 	{
-
+		Orientations.add(Orientation.DEFAULT);
 	}
 
+	/**
+	 * Build - When the MapBuilder has finished initializing all necessary variables, this is called to build the map
+	 * 
+	 * @return - Map that is generated
+	 */
 	public Map Build()
 	{
-		Max_Mass = Dimension / 16;
+		Max_Mass = (int) (Math.round(Math.log10((double) Dimension) / Math.log10(2)) * 2);
 
 		Map = game.Map.GetInstance();
 
 		Land();
+		Sea();
 
 		return Map;
 	}
 
+	/**
+	 * Land - Initializes all tiles in Map.MapTiles and sets them all to Grass
+	 */
 	private void Land()
 	{
 		int SizeActual = Dimension * Dimension;
@@ -151,5 +202,110 @@ public class Mapbuilder
 		Map.Resize(Dimension * Map.TileSize, Dimension * Map.TileSize);
 	}
 
+	private void Sea()
+	{
 
+		Orientation O = GenerateOrient();
+		O = Orientation.NORTH;
+
+		if (O != Orientation.DEFAULT)
+		{
+			Ocean.Init(O, Tile.Type.WATER);
+
+			for (int i = 1; i < Ocean.EndX.length; i++)
+			{
+
+				switch (Ocean.Orientation)
+				{
+					case NORTH:
+						for(int y = Ocean.EndY[0]; y < Ocean.EndY[i]; y++)
+						{
+							for(int x = Ocean.EndX[i - 1]; x < Ocean.EndX[i]; x++)
+							{
+								Map.MapTiles[Map.GetTileIndex(x, y)].ChangeType(Ocean.TType);
+							}
+						}
+						break;
+					case SOUTH:
+						break;
+					case EAST:
+						break;
+					case WEST:
+						break;
+					case DEFAULT:
+					case CENTER:
+						Logging.getInstance().Write(Type.WARNING, "Something Terrible has happened while generating OCEAN in MapBuilder");
+						break;
+				}
+			}
+		}
+	}
+
+	private Orientation GenerateOrient()
+	{
+		int rand = -1;
+		int i = 0;
+
+		Orientation O = Orientation.DEFAULT;
+
+		while (!Orientations.contains(O))
+		{
+			rand = Rand.nextInt(4);
+
+			switch (rand)
+			{
+				case 0:
+					O = Orientation.NORTH;
+					break;
+				case 1:
+					O = Orientation.SOUTH;
+					break;
+				case 2:
+					O = Orientation.EAST;
+					break;
+				case 3:
+					O = Orientation.WEST;
+					break;
+				default:
+					O = Orientation.SOUTH;
+					break;
+			}
+
+			i++;
+
+			if (i == 3)
+			{
+				O = Orientation.DEFAULT;
+				break;
+			}
+		}
+
+		return O;
+	}
+
+	/**
+	 * Lock Orientation - Once a Land Mass is Created we need to lock its orientation so no other can use it
+	 * 
+	 * @param Orient
+	 *            - the orientation that is used
+	 */
+	private void LockOrient(Orientation Orient)
+	{
+		Orientations.add(Orient);
+	}
+
+	/**
+	 * Check Orientation - Determine if check is contained in the Enum Set
+	 * 
+	 * @param check
+	 *            - The orientation in question
+	 * @return - True if the Enum Set contains that flag, false otherwise
+	 */
+	private boolean CheckOrient(Orientation check)
+	{
+		if (Orientations.contains(check))
+			return true;
+
+		return false;
+	}
 }
